963fd7d644151685bb849c37b396cb1245bd2abe
Florian <florian.chevallier03@gmail.com>
2017-05-01 15:33:50 +0200

    Ajout du Hot Module Replacement

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './Home/Home';
+import { AppContainer } from 'react-hot-loader';
+
+import Home from './Home/Home.js';
 import './app.css';
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('maSuperApp'),
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('maSuperApp'),
+  );
+};
 
+render(Home);
+
+// Permet de répondre à la question :
+// Qu'est-ce que je fais lors du Hot Reload ?
+if (module.hot) module.hot.accept('./Home/Home.js', () => render(Home));
