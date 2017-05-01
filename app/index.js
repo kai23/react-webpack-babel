1666671a367f9ea9efcdbeee52e87133cbe6318d
Florian <florian.chevallier03@gmail.com>
2017-05-01 23:18:45 +0200

    ajout des routes avec react-router

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { AppContainer } from 'react-hot-loader';
 
-import Home from './Home/Home.js';
+import App from './App.js';
 import './app.scss';
 
 const render = (Component) => {
@@ -15,8 +15,8 @@ const render = (Component) => {
   );
 };
 
-render(Home);
+render(App);
 
 // Permet de répondre à la question :
 // Qu'est-ce que je fais lors du Hot Reload ?
-if (module.hot) module.hot.accept('./Home/Home.js', () => render(Home));
+if (module.hot) module.hot.accept('./App.js', () => render(App));
